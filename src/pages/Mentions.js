a2db18d493e23ff085bbc08d98d4de07ec4479d7
Miradji-Ladhidhou <ladhidhoum@gmail.com>
2025-03-02 22:53:22 +0400

    develop accordion

--- src/pages/Mentions.js
+++ src/pages/Mentions.js
@@ -1,5 +1,4 @@
 import Accordion from 'react-bootstrap/Accordion';
-import { Link } from 'react-router-dom';
 
 function Mentions() {
   return (
@@ -30,8 +29,8 @@ function Mentions() {
             <Accordion.Header>Crédits</Accordion.Header>
             <Accordion.Body>
             <p>Ce site a été réalisé par John Doe, étudient au <a href="https://www.centre-europeen-formation.fr/accueil/?mrasn=1390966.1726414.o8OuYOxV">Centre Européen de Fromation</a>.</p>
-            <p>Les images utilisées sur ce site sont libres de droits et ont été otenues sur le site <a href="https://www.centre-europeen-formation.fr/accueil/?mrasn=1390966.1726414.o8OuYOxV">Centre Européen de Fromation</a>.</p>
-
+            <p>Les images utilisées sur ce site sont libres de droits et ont été otenues sur le site <a href="https://pixabay.com/fr/">Pixabay</a>.</p>
+            <p>La favicon de ce site a été fournie par<a href="https://www.flaticon.com/fr/">John doe icons erstellt von Freepik - Flaticon</a>.</p>
             </Accordion.Body>
         </Accordion.Item>
         </Accordion>
